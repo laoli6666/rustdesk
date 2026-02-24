f8937c5e71584a8fe5ff6cae81e5a83ef46d96ef
laoli6666 <52724395+laoli6666@users.noreply.github.com>
2026-02-24 17:06:56 +0800

    remove alert

--- flutter/lib/mobile/pages/server_page.dart
+++ flutter/lib/mobile/pages/server_page.dart
@@ -253,13 +253,7 @@ class ServiceNotRunningNotification extends StatelessWidget {
             ElevatedButton.icon(
                 icon: const Icon(Icons.play_arrow),
                 onPressed: () {
-                  if (gFFI.userModel.userName.value.isEmpty &&
-                      bind.mainGetLocalOption(key: "show-scam-warning") !=
-                          "N") {
-                    showScamWarning(context, serverModel);
-                  } else {
                     serverModel.toggleService();
-                  }
                 },
                 label: Text(translate("Start service")))
           ],
