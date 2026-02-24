24e60c4ad7b7bc41bf92c249184ffab027b522d0
laoli6666 <496699211@qq.com>
2026-02-24 09:57:41 +0800

    Change ID

--- flutter/android/app/src/main/kotlin/com/cnhnys/yuankong/InputService.kt
+++ flutter/android/app/src/main/kotlin/com/cnhnys/yuankong/InputService.kt
@@ -1,4 +1,4 @@
-package com.carriez.flutter_hbb
+package com.cnhnys.yuankong
 
 /**
  * Handle remote input and dispatch android gesture
--- flutter/android/app/src/main/kotlin/com/cnhnys/yuankong/MainActivity.kt
+++ flutter/android/app/src/main/kotlin/com/cnhnys/yuankong/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.carriez.flutter_hbb
+package com.cnhnys.yuankong
 
 /**
  * Handle events from flutter
